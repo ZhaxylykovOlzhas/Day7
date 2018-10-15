a051be21234e8fed3ffe1ac4adda3fadcbb3f00a
Жаксылыков Олжас Батырулы <@corp.itstep>
2018-10-15 20:01:42 +0600

    first commit

---
+++ OperatorOverload/OperatorOverload/EqualityTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    class EqualityTest
+    {
+        public string Name { get; set; }
+
+        public bool Equals(EqualityTest other)
+        {
+            return !ReferenceEquals(null, other) && (ReferenceEquals(this, other) || other.Name == Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as EqualityTest);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Name != null ? Name.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(EqualityTest left, EqualityTest right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(EqualityTest left, EqualityTest right)
+        {
+            return !Equals(left, right);
+        }
+    }
+}
---
+++ OperatorOverload/OperatorOverload/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EqualityTest equalityTest1 = new EqualityTest();
+            EqualityTest equalityTest2 = new EqualityTest();
+            Console.WriteLine("Введите 1-имя:");
+            equalityTest1.Name = Console.ReadLine();
+            Console.WriteLine("Введите 2-имя:");
+            equalityTest2.Name = Console.ReadLine();
+
+            Console.WriteLine("Проверка на равенства /если равен вернеть-true, если нет то-false:  " + (equalityTest1 == equalityTest2));
+
+            Console.ReadKey();
+        }
+    }
+}
