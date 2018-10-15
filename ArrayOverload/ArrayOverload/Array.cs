f56d5dc2ff8ac18eb6dd9d79877a47f546093857
Жаксылыков Олжас Батырулы <@corp.itstep>
2018-10-15 19:59:28 +0600

    first commit

---
+++ ArrayOverload/ArrayOverload/Array.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOverload
+{
+    class Array
+    {
+        int[] intArray; 
+        int n;
+        public Array(int[] a, int b) 
+        {
+            intArray = a;
+            n = b;
+        }
+        public void Razmer()
+        {
+            Console.Write("Введите элементы массива: ");
+            for (int i = 0; i < n; i++)
+                intArray[i] = int.Parse(Console.ReadLine()); 
+        }
+
+        public void Print() 
+        {
+            Console.Write("Массив: ");
+            int count = 0; 
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(intArray[i] + " ");
+                count++;
+            }
+            if (count == 0)
+                Console.WriteLine("Массив пуст");
+        }
+
+        public void Sortirovka()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Отсортированный массив:");
+            for (int i = 0; i < n; i++) 
+            {
+                for (int j = 0; j < n - 1; j++) 
+                {
+                    if (intArray[j] > intArray[j + 1]) 
+                    {
+                        int z = intArray[j]; 
+                        intArray[j] = intArray[j + 1];
+                        intArray[j + 1] = z; 
+                    }
+                }
+            }
+        }
+
+        public static Array operator +(Array a, Array b)
+        {
+            Console.WriteLine();
+            Console.WriteLine("+");
+
+            if (a.n != b.n)
+                throw new InvalidOperationException();
+            var res = new Array(new int[a.n], a.n);
+            for (int i = 0; i < a.n; i++)
+            {
+                res.intArray[i] = a.intArray[i] + b.intArray[i];
+            }
+            return res;  
+        }
+
+    }
+}
---
+++ ArrayOverload/ArrayOverload/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOverload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            Console.WriteLine("Введите размерность 1-массива ");
+            N = int.Parse(Console.ReadLine());
+
+
+          
+            int[] A = new int[N];
+            Array massiv = new Array(A, N); 
+            massiv.Razmer();
+            massiv.Print();
+            Array s = new Array(A, A.Length); 
+            s.Sortirovka(); 
+            s.Print();
+
+
+            int a = 0;
+            for (int i = 0; i < A.Length; i++)
+            {
+                a += A[i];
+            }
+            Console.WriteLine("\nCумма 1-массива = " + a);
+
+
+
+            int N1;
+            Console.WriteLine("\n\nВведите размерность 2-массива ");
+            N1 = int.Parse(Console.ReadLine());
+
+
+            int[] A1 = new int[N1];
+            Array massiv1 = new Array(A1, N1);
+            massiv1.Razmer();
+            massiv1.Print();
+            Array s1 = new Array(A1, A1.Length);
+            s1.Sortirovka();
+            s1.Print();
+
+
+            int a1 = 0;
+            for (int i = 0; i < A1.Length; i++)
+            {
+                a1 += A1[i];
+            }
+            Console.WriteLine("\nCумма 2-массива = " + a1);
+
+            bool lengthСheck = false;
+
+            if (a > a1)
+            {
+                lengthСheck = true;
+                Console.WriteLine("\n\n\n"+lengthСheck);
+            }
+            else
+            {
+                Console.WriteLine("\n\n\n" + lengthСheck);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
